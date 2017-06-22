1658475c0f11da7c6c24c1db53a74e25aa40de6f
cainanalves <cainan.teixeira10@hotmail.com>
2017-06-22 16:37:06 -0300

    prototype carro

---
+++ src/controller/CarroClone.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import bean.Carro;
+
+/**
+ *
+ * @author cainan
+ */
+public class CarroClone extends Carro{
+
+    public CarroClone(String placa, String modelo, String marca, String cor) {
+        super(placa, modelo, marca, cor);
+    }
+
+    @Override
+    public Carro clonar(String placa, String modelo, String marca, String cor) {
+        return new CarroClone(placa, modelo, marca, cor);
+    }
+    
+}
