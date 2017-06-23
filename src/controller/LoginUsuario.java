6e1233f1f1e9372569942c13d92a0cfb3cc03238
ArthurGorgonio <gorgonioarthur@gmail.com>
2017-06-23 18:36:00 -0300

    Espaçamento e garantindo que sempre será letras minúsculas

--- src/controller/LoginUsuario.java
+++ src/controller/LoginUsuario.java
@@ -7,25 +7,24 @@ package controller;
 
 import bean.Cliente;
 import bean.Motorista;
-import bean.Usuario;
-import bean.UsuarioInterface;
+import interfaces.UsuarioInterface;
 
 /**
  *
  * @author cainan
  */
 public class LoginUsuario {
-    private UsuarioInterface user;
+  private UsuarioInterface user;
     
-    public LoginUsuario(String usuario){
-        if(usuario == "cliente"){
-            user = new Cliente();
-            System.out.println(user.logarUsuario());
-        }else if(usuario == "motorista"){
-            user = new Motorista();
-            System.out.println(user.logarUsuario());
-        }else
-            System.out.println("Usuário inválido. Você precisa ser 'cliente' ou 'motorista' !");
-        
-    }
+  public LoginUsuario(String usuario){
+    usuario = usuario.toLowerCase();
+    if(usuario == "cliente"){
+      user = new Cliente();
+      System.out.println(user.logarUsuario());
+    }else if(usuario == "motorista"){
+      user = new Motorista();
+      System.out.println(user.logarUsuario());
+    }else
+      System.out.println("Usuário inválido. Você precisa ser 'cliente' ou 'motorista' !");
+  }
 }
