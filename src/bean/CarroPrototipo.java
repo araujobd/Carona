0b81eea24b56fd83012bb1487a894eff10df5dc4
ArthurGorgonio <gorgonioarthur@gmail.com>
2017-06-23 18:36:27 -0300

    Resolvendo o conflito que existe

--- src/bean/CarroPrototipo.java
+++ src/bean/CarroPrototipo.java
@@ -8,20 +8,18 @@ package bean;
 import bean.Carro;
 
 /**
-<<<<<<< HEAD
- *
  * @author cainan
  * Padrão Prototype
  */
 public class CarroPrototipo extends Carro{
 
-    public CarroPrototipo(String placa, String modelo, String marca, String cor) {
-        super(placa, modelo, marca, cor);
-    }
+  public CarroPrototipo(String placa, String modelo, String marca, String cor) {
+    super(placa, modelo, marca, cor);
+  }
 
-    @Override
-    public Carro clonar(String placa, String modelo, String marca, String cor) {
-        return new CarroPrototipo(placa, modelo, marca, cor);
-    }
+  @Override
+  public Carro clonar(String placa, String modelo, String marca, String cor) {
+    return new CarroPrototipo(placa, modelo, marca, cor);
+  }
 
 }
