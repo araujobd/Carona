29ed395c5e46ff2e78699f22ef1e07f967de5adf
ArthurGorgonio <gorgonioarthur@gmail.com>
2017-06-25 17:16:40 -0300

    Criando carros e fazendo o uso dos padrões determinados pelo grupo

--- src/main/Principal.java
+++ src/main/Principal.java
@@ -5,23 +5,78 @@
  */
 package main;
 
-import bean.Log;
+import java.util.ArrayList;
+
+import bean.Carro;
+import bean.CarroPrototipo;
 import controller.LoginUsuario;
+import decoradores.Airbag;
+import decoradores.ArCondicionado;
+import decoradores.CarroDecorado;
+import decoradores.Seguro;
+import decoradores.VidroEletrico;
+import log.Log;
 
 /**
- *
  * @author cainan
+ * 
  */
 public class Principal {
   public static void main(String[] args){
+    // Singleton
     Log log = null;
     log = Log.getInstancia();
     log.escreve();
+    
+    //Strategy + (facade & singleton) [encapsulados]
     LoginUsuario u1 = new LoginUsuario("cliente");
     log.escreve();
     LoginUsuario u2 = new LoginUsuario("motorista");
     log.escreve();
     LoginUsuario u3 = new LoginUsuario("nenhum dos dois");
     log.escreve();
+    
+    //Carro
+    ArrayList<CarroPrototipo> carros = new ArrayList<CarroPrototipo>();
+    Carro carro = new Carro();
+        
+    //setando carro
+    carro.setPlaca("QWE1234");
+    carro.setMarca("BMW");
+    carro.setModelo("M3");
+    carro.setCor("Branca");
+    
+    //adicionando no arrayList
+    carros.add(new CarroPrototipo(carro));
+    
+    //setando outro carro
+    carro.setPlaca("ABC0000");
+    carro.setMarca("Gallardo");
+    carro.setModelo("Lamborghini");
+    carro.setCor("Rosa-Chock");
+    
+    carros.add(new CarroPrototipo(carro));
+    
+    for (Carro c : carros) {
+      System.out.println("Placa: " + c.getPlaca()
+                      + "\nMarca: " + c.getMarca()
+                      + "\nModelo: " + c.getModelo()
+                      + "\nCor: " + c.getCor() + "\n");
+    }
+    
+    //Decorando os carros
+    //Decorando o BMW com Airbag e Vidros Elétricos
+    System.out.println(carros.get(0).getModelo());
+    CarroDecorado decorado = new Airbag(carros.get(0));
+    decorado.montar();
+    decorado = new VidroEletrico(carros.get(0));
+    decorado.montar();
+    
+    System.out.println("\n\n" + carros.get(1).getModelo());
+    decorado = new ArCondicionado(carros.get(1));
+    decorado.montar();
+    decorado = new Seguro(carros.get(1));
+    decorado.montar();
   }
 }
+
