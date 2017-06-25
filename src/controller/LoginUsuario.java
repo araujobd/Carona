00687abce9c77fb14ca92783206bfab8c30a57e7
ArthurGorgonio <gorgonioarthur@gmail.com>
2017-06-25 17:10:29 -0300

    Utilizando o DAO do usuario

--- src/controller/LoginUsuario.java
+++ src/controller/LoginUsuario.java
@@ -7,24 +7,34 @@ package controller;
 
 import bean.Cliente;
 import bean.Motorista;
+import bean.Usuario;
+import dao.UsuarioDAO;
 import interfaces.UsuarioInterface;
 
 /**
- *
  * @author cainan
  */
 public class LoginUsuario {
   private UsuarioInterface user;
+  private UsuarioDAO dao = null;
     
   public LoginUsuario(String usuario){
+    dao = UsuarioDAO.getInstance(); // criando o dao
     usuario = usuario.toLowerCase();
+
     if(usuario == "cliente"){
+      
       user = new Cliente();
+      dao.adicionar((Usuario) user);
       System.out.println(user.logarUsuario());
+    
     }else if(usuario == "motorista"){
+    
       user = new Motorista();
       System.out.println(user.logarUsuario());
+    
     }else
-      System.out.println("Usuário inválido. Você precisa ser 'cliente' ou 'motorista' !");
+      System.out.println("Usuário inválido. Você precisa ser 'cliente' ou 'motorista' !\n");
   }
+  
 }
